8c7859c2e4dcc2beb6e6bd039d180f2533b7c5de
AgustinItuarte <agustin242536@gmail.com>
2023-12-22 22:53:41 -0300

    Hook to save buyed items added

--- shopping-cart/src/components/Shop/BuyItem.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-const buyItem = (item) => {
-
-    console.log(item)
-
-}
-
-export default buyItem;
--- shopping-cart/src/components/Shop/Shop.jsx
+++ shopping-cart/src/components/Shop/Shop.jsx
@@ -1,9 +1,11 @@
 import GetData from "./GetData";
-import buyItem from "./BuyItem";
+import { useState } from "react";
 
 const Shop = () => {
 
+    const [buyedItems, setBuyedItems] = useState([])
     const { items, error, loading } = GetData ();
+    console.log(buyedItems)
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error loading the page</div>;
@@ -14,7 +16,7 @@ const Shop = () => {
             return(
                 <div key={item.id} className="shop-item">
                     <p>Name</p>
-                    <button onClick={() => buyItem(item)}>Add to cart</button>
+                    <button onClick={() => setBuyedItems(item)}>Add to cart</button>
                 </div>
             )
     
