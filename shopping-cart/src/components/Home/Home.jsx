36689fea94166eb0b2e441da0e52bfbfdc16af41
AgustinItuarte <agustin242536@gmail.com>
2025-03-02 20:59:55 -0300

    Vite Framework installed

--- shopping-cart/src/components/Home/Home.jsx
+++ shopping-cart/src/components/Home/Home.jsx
@@ -1,29 +1,6 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
-/* const Home = () => {
-
-    const {name} = useParams();
-
-    return(
-        <div>
-            <ul>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/shop"}>Shop</Link></li>
-                <li><Link to={"/cart"}>Cart</Link></li>
-            </ul>
-
-            {name === "shop" ? (
-                <Shop/>
-            ): name === "cart" ?(
-                <h1>Cart</h1>
-            ) : (
-                <h1>Default Profile</h1>
-            )}    
-        </div>
-    )
-} */
-
 function Home() {
     return(
         <div className="container">
---
+++ shopping-cart/src/tests/App.test.jsx
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest';
+/* import { Home } from "Home"; */
+
+describe('something truthy and falsy', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
+});
